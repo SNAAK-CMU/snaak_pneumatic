67c5665e5a0af66c14c2474db32784b2b9d5192c
RodrigoCatto <rodrigo.catto7@gmail.com>
2025-02-21 20:48:31 -0500

    Enable serial comunication

--- pneumatic_control/pneumatic_control.ino
+++ pneumatic_control/pneumatic_control.ino
@@ -1,55 +1,74 @@
 #include "ClearCore.h"
 
+#define InputConnector ConnectorIO2
 #define ENABLE_VACUUM_PIN IO0
-#define EJECT_VACCUM_PIN IO1
+#define EJECT_VACUUM_PIN IO1
 #define ENABLE_BTN_PIN IO2
 #define EJECT_BTN_PIN IO3
 
-#define InputConnector ConnectorIO2
+#define baudRateSerialPort 115200
+#define baudRateInputPort 115200
+#define isTtlInputPort false
 
 bool enableButtonValue;
 PinStatus ejectButtonValue;
 
 bool isEnable = false;
+int input;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(ENABLE_VACUUM_PIN, OUTPUT);
-  pinMode(EJECT_VACCUM_PIN, OUTPUT);
+  // Initialize Serial
+  Serial.begin(baudRateSerialPort); // Initialize serial communication with ROS2
+  while (!Serial) {
+    continue;
+  }
 
+  Serial.ttl(isTtlInputPort);
+  while (!Serial0) {
+    continue;
+  }
+
+  // Define inputs and outputs
+  pinMode(ENABLE_VACUUM_PIN, OUTPUT);
+  pinMode(EJECT_VACUUM_PIN, OUTPUT);
   pinMode(ENABLE_BTN_PIN, INPUT);
   pinMode(EJECT_BTN_PIN, INPUT);
 
+  // Reset Pneumatic Ports
+  digitalWrite(ENABLE_VACUUM_PIN, LOW);
+  digitalWrite(EJECT_VACUUM_PIN, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Read Buttons Values
   enableButtonValue = InputConnector.InputRisen();
   ejectButtonValue = digitalRead(EJECT_BTN_PIN);
 
-  if((isEnable == false) && (enableButtonValue == true)){
-    isEnable = true;
-    enableButtonValue = 0;
+  // Read Serial
+  input = Serial.read();
 
+  if (input != -1) {
+    String command = "";
+    while (Serial.available()) {
+      command += (char)input;
+      input = Serial.read();
+    }
+    
+    if (command == "enable" || isEnable) {
+      isEnable = true;
+      digitalWrite(ENABLE_VACUUM_PIN, HIGH);
+    } 
+    if (isEnable && command == "disable") {
+      isEnable = false;
+      digitalWrite(ENABLE_VACUUM_PIN, LOW);
+    } 
+    if (command.startsWith("eject")) {
+      int duration = command.substring(5).toInt();
+      digitalWrite(EJECT_VACUUM_PIN, HIGH);
+      delay(duration);
+      digitalWrite(EJECT_VACUUM_PIN, LOW);
+    }
   }
-
-  if((isEnable == true) && (enableButtonValue == true)){
-    isEnable = false;
-    enableButtonValue = 0;
-  }
-
-  if(isEnable){
-    digitalWrite(ENABLE_VACUUM_PIN, HIGH);
-  } else {
-    digitalWrite(ENABLE_VACUUM_PIN, LOW);
-  }
-
-
-  if(ejectButtonValue){
-    digitalWrite(EJECT_VACCUM_PIN, HIGH);
-  } else {
-    digitalWrite(EJECT_VACCUM_PIN, LOW);
-  }
-
-  delay(100);
+  
+  delay(10);
 }
